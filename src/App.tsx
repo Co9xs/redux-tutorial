b29717e5e6f923bc77cb62ee55a0d5071f7a198b
Co9xs <ryo.fjsm0203@icloud.com>
2021-06-25 09:19:36 +0900

    add settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { HomeContainer as Home } from "./containers/Home";
+import { Provider } from 'react-redux';
+import { store } from "./store"
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
       <Home />
-    </div>
+    </Provider>
   );
 }
 
